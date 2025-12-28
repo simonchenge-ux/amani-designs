83b921540a1c2369d73241aca99a71cf9cd7ff06
s1monchenge-ux <simonchenge21@gmail.com>
2025-12-28 16:09:49 +0300

    javascript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+function updateWallPrice(select) {
+  const meters = parseInt(select.value);
+  document.getElementById("wall-price").innerText = meters * 3000;
+}
+
+function updateSheerPrice(input) {
+  const meters = parseInt(input.value);
+  document.getElementById("sheer-price").innerText = meters * 500;
+}
 
